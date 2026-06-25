cdbd926211a0431d2d41fcca843857df98c2cc2d
694259014-gif <694259014@webmail.npru.ac.th>
2026-06-25 16:21:05 +0700

    nameage.java

---
+++ nameage.java
@@ -0,0 +1,9 @@
+void main() {
+    IO.println("enter your name");
+    String name = IO.readln();
+
+    IO.println("enter your age");
+    int age = Integer.parseInt(IO.readln());
+
+    IO.println("Hello " + name + " . Your age is " + age);
+}
