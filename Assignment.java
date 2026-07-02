d199e7ccb6721d1810caa1e3558750c14e4c4443
694259014-gif <694259014@webmai.npru.ac.th>
2026-07-02 15:23:53 +0700

    First commit: Add Assignment files

---
+++ Assignment.java
@@ -0,0 +1,11 @@
+void main() {
+       IO.print("Enter first number: ");
+       int num1 = Integer.parseInt(IO.readln());
+
+       IO.print("Enter second number: ");
+       int num2 = Integer.parseInt(IO.readln());
+
+       int sum = num1 + num2;
+
+       IO.println("Sum of " + num1 + " and " + num2 + " = " + sum);
+}
