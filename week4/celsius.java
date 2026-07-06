d39455bb031f0f6e74b70e729f2d3561e4611ca8
694259014-gif <694259014@webmail.npru.ac.th>
2026-07-06 15:58:46 +0700

    Celsius.java

---
+++ week4/celsius.java
@@ -0,0 +1,13 @@
+void main() {
+
+    // 1. ja(รับข้อมูลอุณหภูมิฟาเรนไฮต์)
+    IO.print("Enter Fahrenheit: ");
+    double fahrenheit = Double.parseDouble(IO.readln());
+
+    // 2. (คำนวณแปลงค่าเป็นองศาเซลเซียส)
+    double celsius = (5.0 / 9.0) * (fahrenheit - 32);
+
+    // 3. (แสดงผลลัพธ์)
+    IO.println("Celsius = " + celsius);
+
+}
