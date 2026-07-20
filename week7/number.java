fd7364d7312cbaec6585cce9556b5f051af6fcea
694259014-gif <694259014@webmail.npru.ac.th>
2026-07-20 14:15:20 +0700

    อัปเดตระบบรับค่าตัวเลข

---
+++ week7/number.java
@@ -0,0 +1,11 @@
+void main() {
+
+    String input = IO.readln("enter your number: ");
+    int number = Integer.parseInt(input);
+
+    if (number > 0) {
+        IO.println("จำนวนเต็มบวก");
+    }
+
+    IO.println("จบโปรแกรม");
+}
