6de8271dbc9ad6966cea4f89c26070f0bd970b46
694259014-gif <694259014@webmail.npru.ac.th>
2026-07-20 16:12:33 +0700

    การบ้าน3

--- week7/hw3.java
+++ week7/hw3.java
@@ -1,5 +1,20 @@
-package ComputerProblemSolving.week7;
+void main() {
+    String gender = IO.readln("Enter gender (M/F): ");
+    double weight = Double.parseDouble(IO.readln("Enter weight (kg): "));
+    double height = Double.parseDouble(IO.readln("Enter height (cm): "));
 
-public class hw3 {
+    double age = Double.parseDouble(IO.readln("Enter age (years): "));
 
+    double bmr = 0;
+
+    if (gender.equalsIgnoreCase("M")) {
+        bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
+    } else if (gender.equalsIgnoreCase("F")) {
+        bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
+    } else {
+        IO.println("Invalid gender input!");
+        return;
+    }
+
+    IO.println("BMR: " + bmr);
 }
