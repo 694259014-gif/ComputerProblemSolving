1722d92f6e98337d94daff52dbc8e96d2020b150
694259014-gif <694259014@webmail.npru.ac.th>
2026-07-20 14:41:02 +0700

    true false

---
+++ week7/Oddeven.java
@@ -0,0 +1,9 @@
+void main() {
+    int number = Integer.parseInt(IO.readln("Enter a number: "));
+
+    if (number % 2 == 0) {
+        IO.println("The number " + number + " is Even.");
+    } else {
+        IO.println("The number " + number + " is Odd");
+    }
+}
